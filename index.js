64f9e055fd4d0be30cc77e1be2eb39d8866c2981
akshatphumbhra <43840267+akshatphumbhra@users.noreply.github.com>
2020-09-24 05:14:58 +0530

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const run = function () {
     // Exercise 2
-
+    document.getElementById("ptag-id").style.backgroundColor = "red";
     // Exercise 3 - Extra Credit
     const fruits = ['Apple', 'Banana', 'Orange'];
-    
+    fruitCart = document.createElement('ol');
+    fruitCart.textContent = "Fruit Cart";
+    var i;
+    for(i = 0; i<fruits.length; i++){
+      var fruit = document.createElement("li");
+      fruit.id = "fruit";
+      fruit.innerText = fruits[i];
+      fruitCart.appendChild(fruit);
+    }
+
+    document.getElementById("shopping-cart").appendChild(fruitCart);
     // hint 1: how do we loop through items in JavaScript
 
     // hint 2: use how do we create an element and append an element?
